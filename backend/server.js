9c6b48dbe75a00a0f56eac6e9ddd8aa460674b3b
adiqa <adiqarain.biz@gmail.com>
2026-05-17 02:25:47 +0300

    debug: add uncaught exception handlers to surface startup errors

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
+  process.exit(1);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err);
+  process.exit(1);
+});
+
 require('dotenv').config();
 const app = require('./src/app');
 const { startBreakageScheduler } = require('./src/services/breakageService');
